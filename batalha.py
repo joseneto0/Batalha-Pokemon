fca7852329efb851c5a0b2fca1c437d00162fbf5
José-Neto <joseneto.jn1606@gmail.com>
2022-05-28 13:05:42 -0300

    v1.3

--- batalha.py
+++ batalha.py
@@ -4,22 +4,23 @@ import funções
 
 
 while True:
+    nomes = ['Charizard', 'Blastoise', 'Venusaur']
+
+    vida_pokemons = [70, 70, 70]
+
     charizard_ataques = {"Air Slash": 10, "Heat Wave": 14, "Dragon Claw": 10, "Flare Blitz": 8}
     limites_char = [8, 3, 4, 10]
-    vida_charizard = 70
 
     rayquaza_ataques = {"Hyper Beam": 12, "Ice Beam": 10, "Outrage": 15, "Seismic Toss": 10}
-    vida_rayquaza = 70
     limites_ray = [3, 4, 2, 4]
+    vida_rayquaza = 70
     jogada_rayquaza = randint(0, 3)
 
     blastoise_ataques = {"Hydro Pump": 15, "Water Gun": 8, "Hydro Cannon": 11, "Ice Beam": 12}
     limites_blas = [2, 10, 4, 3]
-    vida_blastoise = 70
 
     venusaur_ataques = {"Vine Whip": 13, "Solar Beam": 16, "Razor Leaf": 10, "Tackle": 8}
     limites_ven = [1, 1, 5, 5]
-    vida_venusaur = 70
 
     funções.linha()
     print(f'{"BATALHA POKEMON":^40}')
@@ -31,27 +32,11 @@ while True:
     """)
     escolha = int(input('Digite sua escolha: '))
     while escolha < 1 or escolha > 3:
-        escolha = int(input('Pô mano, escolha certo :). Digite sua escolha: '))
+        escolha = int(input('Pô mano, digite um número válido :). Digite sua escolha: '))
 
     #charizard
     if escolha == 1:
-        funções.linha()
-        print('\033[1;31mParabéns! Você escolheu o Charizard\033[m')
-        sleep(1)
-        print(f'O seu pokemon tem {vida_charizard} de vida')
-        sleep(1)
-        print('Os ataques deles são: ')
-        for i, e in enumerate(charizard_ataques):
-            print(f'{i} - {e}: {charizard_ataques[e]} de ataque, com limite de {limites_char[i]} vezes')
-            sleep(1)
-        funções.linha()
-        print("OH NÃO!")
-        sleep(1)
-        print("\033[1;32mUM RAYQUAZA APARECEU!\033[m")
-        sleep(1)
-        print("\033[1;31mLUTE PELA SUA VIDA, MESTRE POKEMON\033[m")
-        sleep(1)
-        print(f'O pokemon RAYQUAZA tem {vida_rayquaza} de vida')
+        funções.inicio(escolha, nomes, vida_pokemons, charizard_ataques, limites_char)
 
         #Pegar os valores do dicionario (ataque e dano do ataque)
         #charizard
@@ -68,14 +53,7 @@ while True:
             funções.linha()
             sleep(0.7)
             print('\033[1;31mVEZ DO CHARIZARD\033[m')
-            ataque = int(input('Qual vai ser seu ataque? [4 - rever os ataques] '))
-            ataque = funções.ataque_4(ataque, charizard_ataques)
-            while ataque < 0 or ataque > 4:
-                ataque = int(input('\033[1;31mNÚMERO INVÁLIDO.\033[m Qual vai ser seu ataque? [4 - rever os ataques] '))
-            if ataque == 0 or ataque == 1 or ataque == 2 or ataque == 3:
-                while limites_char[ataque] == 0:
-                    ataque = int(input('\033[1;31mLIMITE ZERADO\033[m. Escolha outro ataque: [4 - rever os ataques] '))
-                    ataque = funções.ataque_4(ataque, charizard_ataques)
+            ataque = funções.ataque(charizard_ataques, limites_char)
             sleep(0.7)
             print(f'Você ativou o ataque {keys_char[ataque]}, que da {values_char[ataque]} de dano, você pode utiliza-lo {limites_char[ataque]-1} vezes')
             limites_char[ataque] -= 1
@@ -97,9 +75,9 @@ while True:
             while limites_ray[jogada_rayquaza] == 0:
                 jogada_rayquaza = randint(0, 3)
             sleep(0.7)
-            print(f'Agora o Charizard está com \033[1;31m{funções.vida(vida_charizard, values_ray[jogada_rayquaza])}\033[m de vida')
-            vida_charizard = funções.vida(vida_charizard, values_ray[jogada_rayquaza])
-            if vida_charizard == 0:
+            print(f'Agora o Charizard está com \033[1;31m{funções.vida(vida_pokemons[escolha - 1], values_ray[jogada_rayquaza])}\033[m de vida')
+            vida_pokemons[escolha - 1] = funções.vida(vida_pokemons[escolha - 1], values_ray[jogada_rayquaza])
+            if vida_pokemons[escolha - 1] == 0:
                 funções.linha()
                 print(f'\033=1;31mVOCÊ PERDEU! :C\033[m')
                 break
@@ -107,23 +85,7 @@ while True:
 
     #Blastoise
     if escolha == 2:
-        funções.linha()
-        print('\033[1;34mParabéns! Você escolheu o Blastoise\033[m')
-        sleep(1)
-        print(f'O seu pokemon tem {vida_blastoise} de vida')
-        sleep(1)
-        print('Os ataques deles são: ')
-        for i, e in enumerate(blastoise_ataques):
-            print(f'{i} - {e}: {blastoise_ataques[e]} de ataque, com limite de {limites_blas[i]} vezes')
-            sleep(1)
-        funções.linha()
-        print("OH NÃO!")
-        sleep(1)
-        print("\033[1;31mUM RAYQUAZA APARECEU!\033[m")
-        sleep(1)
-        print("\033[1;31mLUTE PELA SUA VIDA, MESTRE POKEMON\033[m")
-        sleep(1)
-        print(f'O pokemon RAYQUAZA tem {vida_rayquaza} de vida')
+        funções.inicio(escolha, nomes, vida_pokemons, blastoise_ataques, limites_blas)
 
         # Pegar os valores do dicionario (ataque e dano do ataque)
         # blastoise
@@ -140,14 +102,7 @@ while True:
             funções.linha()
             sleep(0.7)
             print('\033[1;34mVEZ DO BLASTOISE\033[m')
-            ataque = int(input('Qual vai ser seu ataque? [4 - rever os ataques] '))
-            ataque = funções.ataque_4(ataque, blastoise_ataques)
-            while ataque < 0 or ataque > 4:
-                ataque = int(input('\033[1;31mNÚMERO INVÁLIDO.\033[m Qual vai ser seu ataque? [4 - rever os ataques] '))
-            if ataque == 0 or ataque == 1 or ataque == 2 or ataque == 3:
-                while limites_blas[ataque] == 0:
-                    ataque = int(input('\033[1;31mLIMITE ZERADO\033[m. Escolha outro ataque: [4 - rever os ataques] '))
-                    ataque = funções.ataque_4(ataque, blastoise_ataques)
+            ataque = funções.ataque(blastoise_ataques, limites_blas)
             sleep(0.7)
             print(f'Você ativou o ataque {keys_blas[ataque]}, que da {values_blas[ataque]} de dano, você pode utiliza-lo {limites_blas[ataque] - 1} vezes')
             limites_blas[ataque] -= 1
@@ -169,9 +124,9 @@ while True:
             while limites_ray[jogada_rayquaza] == 0:
                 jogada_rayquaza = randint(0, 3)
             sleep(0.7)
-            print(f'Agora o Blastoise está com \033[1;31m{funções.vida(vida_blastoise, values_ray[jogada_rayquaza])}\033[m de vida')
-            vida_blastoise = funções.vida(vida_blastoise, values_ray[jogada_rayquaza])
-            if vida_blastoise == 0:
+            print(f'Agora o Blastoise está com \033[1;31m{funções.vida(vida_pokemons[escolha - 1], values_ray[jogada_rayquaza])}\033[m de vida')
+            vida_pokemons[escolha - 1] = funções.vida(vida_pokemons[escolha - 1], values_ray[jogada_rayquaza])
+            if vida_pokemons[escolha - 1] == 0:
                 funções.linha()
                 print(f'\033=1;31mVOCÊ PERDEU! :C\033[m')
                 break
@@ -179,23 +134,7 @@ while True:
 
     #venusaur
     if escolha == 3:
-        funções.linha()
-        print('\033[1;92mParabéns! Você escolheu o Venusaur\033[m')
-        sleep(1)
-        print(f'O seu pokemon tem {vida_venusaur} de vida')
-        sleep(1)
-        print('Os ataques deles são: ')
-        for i, e in enumerate(venusaur_ataques):
-            print(f'{i} - {e}: {venusaur_ataques[e]} de ataque, com limite de {limites_ven[i]} vezes')
-            sleep(1)
-        funções.linha()
-        print("OH NÃO!")
-        sleep(1)
-        print("\033[1;32mUM RAYQUAZA APARECEU!\033[m")
-        sleep(1)
-        print("\033[1;31mLUTE PELA SUA VIDA, MESTRE POKEMON\033[m")
-        sleep(1)
-        print(f'O pokemon RAYQUAZA tem {vida_rayquaza} de vida')
+        funções.inicio(escolha, nomes, vida_pokemons, venusaur_ataques, limites_ven)
 
         #Pegar os valores do dicionario (ataque e dano do ataque)
         #charizard
@@ -212,14 +151,7 @@ while True:
             funções.linha()
             sleep(0.7)
             print('\033[1;92mVEZ DO VENUSAUR\033[m')
-            ataque = int(input('Qual vai ser seu ataque? [4 - rever os ataques] '))
-            ataque = funções.ataque_4(ataque, venusaur_ataques)
-            while ataque < 0 or ataque > 4:
-                ataque = int(input('\033[1;31mNÚMERO INVÁLIDO.\033[m Qual vai ser seu ataque? [4 - rever os ataques] '))
-            if ataque == 0 or ataque == 1 or ataque == 2 or ataque == 3:
-                while limites_ven[ataque] == 0:
-                    ataque = int(input('\033[1;31mLIMITE ZERADO\033[m. Escolha outro ataque: [4 - rever os ataques] '))
-                    ataque = funções.ataque_4(ataque, venusaur_ataques)
+            ataque = funções.ataque(venusaur_ataques, limites_ven)
             sleep(0.7)
             print(f'Você ativou o ataque {keys_ven[ataque]}, que da {values_ven[ataque]} de dano, você pode utiliza-lo {limites_ven[ataque]-1} vezes')
             limites_ven[ataque] -= 1
@@ -241,9 +173,9 @@ while True:
             while limites_ray[jogada_rayquaza] == 0:
                 jogada_rayquaza = randint(0, 3)
             sleep(0.7)
-            print(f'Agora o Venusaur está com \033[1;31m{funções.vida(vida_venusaur, values_ray[jogada_rayquaza])}\033[m de vida')
-            vida_venusaur = funções.vida(vida_venusaur, values_ray[jogada_rayquaza])
-            if vida_venusaur == 0:
+            print(f'Agora o Venusaur está com \033[1;31m{funções.vida(vida_pokemons[escolha - 1], values_ray[jogada_rayquaza])}\033[m de vida')
+            vida_pokemons[escolha - 1] = funções.vida(vida_pokemons[escolha - 1], values_ray[jogada_rayquaza])
+            if vida_pokemons[escolha - 1] == 0:
                 funções.linha()
                 print(f'\033[1;31mVOCÊ PERDEU! :C\033[m')
                 break
--- funções.py
+++ funções.py
@@ -1,3 +1,25 @@
+from time import sleep
+
+
+def inicio(escolha, nome, vida_pokemons, ataques, limites):
+    linha()
+    print(f'\033[1;31mParabéns! Você escolheu o {nome[escolha - 1]}\033[m')
+    sleep(1)
+    print(f'O seu pokemon tem {vida_pokemons[escolha - 1]} de vida')
+    sleep(1)
+    for i, e in enumerate(ataques):
+        print(f'{i} - {e}: {ataques[e]} de ataque, com limite de {limites[i]} vezes')
+        sleep(1)
+    linha()
+    print("OH NÃO!")
+    sleep(1)
+    print("\033[1;32mUM RAYQUAZA APARECEU!\033[m")
+    sleep(1)
+    print("\033[1;31mLUTE PELA SUA VIDA, MESTRE POKEMON\033[m")
+    sleep(1)
+    print(f'O pokemon RAYQUAZA tem 70 de vida')
+
+
 def vida(vida, dano):
     vida -= dano
     if vida <= 0:
@@ -5,13 +27,25 @@ def vida(vida, dano):
     return vida
 
 
-def ataque_4(ataque, ataques_pokemons):
+def ataque(ataques_pokemons, limite=0):
+    ataque = int(input('Qual vai ser seu ataque? [4 - rever os ataques] '))
+    while ataque < 0 or ataque > 4:
+        ataque = int(input('\033[1;31mNÚMERO INVÁLIDO.\033[m Qual vai ser seu ataque? [4 - rever os ataques] '))
     while ataque == 4:
         linha()
         for i, e in enumerate(ataques_pokemons):
             print(f'{i} - {e}: {ataques_pokemons[e]} de ataque')
         linha()
         ataque = int(input('Qual vai ser seu ataque? [4 - rever os ataques] '))
+    if ataque == 0 or ataque == 1 or ataque == 2 or ataque == 3:
+        while limite[ataque] == 0:
+            ataque = int(input('\033[1;31mLIMITE ZERADO\033[m. Escolha outro ataque: [4 - rever os ataques] '))
+            while ataque == 4:
+                linha()
+                for i, e in enumerate(ataques_pokemons):
+                    print(f'{i} - {e}: {ataques_pokemons[e]} de ataque')
+                linha()
+                ataque = int(input('Qual vai ser seu ataque? [4 - rever os ataques] '))
     return ataque
 
 
