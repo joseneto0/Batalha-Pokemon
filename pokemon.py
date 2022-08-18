29fd01bfa5eaf009c9e8bad2998f0f5d098fe47a
Jose <joseneto.jn1606@gmail.com>
2022-08-17 23:45:52 -0300

    v2.1

--- pokemon.py
+++ pokemon.py
@@ -12,10 +12,17 @@ class Pokemon:
 
     def conta(self, atk, dano_atk):
         return ((2 * self.nivel // (5 + 2) * atk * dano_atk // self.defe) // 50) + 2
+<<<<<<< HEAD
     
     def mostrar_ataques(self, ataques, dano):
         for i in range(len(ataques)):
             print(f'[{i+1}] - {ataques[i]} - {dano[i]} de dano'.center(15), flush=True)
+=======
+
+    def mostrar_ataques(self):
+        for i in range(len(self.ataques)):
+            print(f'[{i+1}] - {self.ataques[i]} ~ {self.dano_ataques[i]} de dano'.center(15), flush=True)
+>>>>>>> 6634261 (v2.1)
             sleep(1)
     
     def decrementar_vida(self, v):
